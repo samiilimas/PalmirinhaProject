4b3149b3ca5387f5b5e40afc80c15329d94cf6ec
Sami Lima <sasamiralimaa@gmail.com>
2024-04-04 22:18:54 -0300

    Criador das funções da receita

--- ReceitaApp/views.py
+++ ReceitaApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from ReceitaApp.models import Receita, Categoria
 
 # Create your views here.
+def index(request):
+    categorias = Categoria.objects.all()
+
+    context = {
+        'categorias' : categorias
+    }
+
+    return render(request, 'index.html', context)
+
+def listar_receitas(request):
+    
+    receitas = Receita.objects.all()
+
+    #dicionario que vai levar os dados para a template
+    #chave : valor (ex.: 'cep' : 08010101)
+    
+    context = {
+        'receitas' : receitas
+    }
+
+    #qual template essa view vai retornar
+    return render(request, 'receitas.html', context)
+
+def detalhes_receita(request, id):
+    #buscando a receita correspondente ao id informado
+    receita = Receita.objects.get(id = id)
+    
+    context = {
+        'receita' : receita
+    }
+    return render(request, 'receita.html', context)
